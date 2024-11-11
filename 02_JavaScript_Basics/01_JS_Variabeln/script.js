6519794f1f88aff49ffc819bfec7c822dbaaf425
Leander <leander.wachter@student.htldornbirn.at>
2024-11-11 15:45:18 +0100

    01_JS_Variabeln finished

---
+++ 02_JavaScript_Basics/01_JS_Variabeln/script.js
@@ -0,0 +1,55 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variabeln
+
+let a = 7;
+let b = 8;
+let c = a + b;
+
+console.log("A hat den Wert: " +a);
+
+a = 10;
+
+console.log("A hat den Wert: " +a);
+
+console.log("a + b = " + c);
+
+c = b - a;
+console.log("b -a = " + c);
+
+a = 7;
+c = 10;
+d = a + b + c;
+console.log("a + b + c = " + d);
+
+e = (a+b) / c;
+console.log("(a + b)/c = " + e);
+
+f = c - a + b;
+console.log("c - a + b = " + f);
+
+g = a + b + c *3
+console.log("a + b + c * 3 = " + g);
+
+h = (a + b +c)/4
+console.log("(a + b + c)/4 = " + h);
+
+a = 3;
+b = a+ 3; 
+c = c- 3;
+
+d = a + b + c;
+console.log("a + b + c = " + d);
+
+e = (a+b) / c;
+console.log("(a + b)/c = " + e);
+
+f = c - a + b;
+console.log("c - a + b = " + f);
+
+g = a + b + c *3
+console.log("a + b + c * 3 = " + g);
+
+h = (a + b +c)/4
+console.log("(a + b + c)/4 = " + h);
