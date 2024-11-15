09344e694ae977963413c0a91b509300e3a3a5ef
Leander <leander.wachter@student.htldornbirn.at>
2024-11-15 09:46:18 +0100

    04_JS_Loops finished

---
+++ 02_JavaScript_Basics/04_JS_Loops/script.js
@@ -0,0 +1,49 @@
+
+/*
+for (let i = 7; i <= 45; i++) {
+    console.log(i);
+    
+}
+
+*/
+
+/*
+for (let i = 9; i < 100; i++) {
+    console.log(i);
+    
+}
+*/
+
+
+/*
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+    
+}
+*/
+
+/*
+for (let i = 1234; i >= 1207; i--) {
+    console.log(i);
+    
+}
+*/
+
+/*
+for (let i = 0; i < 9; i++) {
+    console.log("Leander " + i);
+    
+}
+*/
+
+for (let i = 0; i < 11; i++) {
+    console.log(i);
+}
+console.log("Leander ");
+
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+}
+
+
+
