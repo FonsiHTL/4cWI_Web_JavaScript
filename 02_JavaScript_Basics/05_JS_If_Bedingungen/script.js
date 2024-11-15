9d1bcf8c7b9811e5f15fba526a1c02a8d0acc25d
Leander <leander.wachter@student.htldornbirn.at>
2024-11-15 10:12:28 +0100

    06_JS_If_Bedingungen finished

---
+++ 02_JavaScript_Basics/05_JS_If_Bedingungen/script.js
@@ -0,0 +1,27 @@
+let x = 100
+
+
+/*
+if (x < 20) {
+    console.log("Mini");
+
+}
+else if (x >= 20 && x <= 50) {
+    console.log("Medium");
+}
+else if (x > 50){
+    console.log("Large");
+}
+
+*/
+
+if (x <= 7) {
+    console.log("Mini");
+
+}
+else if (x >= 7 && x <= 90) {
+    console.log("Medium");
+}
+else if (x > 90){
+    console.log("Large");
+}
