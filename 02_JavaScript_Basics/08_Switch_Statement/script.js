c851f5fb46e85cefe94bd534cc0456d1a0c35c3f
Leander <leander.wachter@student.htldornbirn.at>
2024-12-02 15:42:22 +0100

    08_Switch_Statement finished

---
+++ 02_JavaScript_Basics/08_Switch_Statement/script.js
@@ -0,0 +1,26 @@
+let randomNumber1 = Math.floor(Math.random() * 6) +5;
+
+
+console.log(randomNumber1);
+
+
+switch (randomNumber1) {
+    case 10:
+        console.log("Ten");
+        break;
+    case 9:
+        console.log("Nine");
+        break;
+    case 8:
+        console.log("Eight");
+        break;
+    case 7:
+        console.log("Seven");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+}
