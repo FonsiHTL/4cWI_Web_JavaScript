4c45492628ecd3267ab9739a1392d62e56bc8801
Leander <leander.wachter@student.htldornbirn.at>
2025-01-10 09:52:55 +0100

    09_ATM finished

--- 02_JavaScript_Basics/09_ATM_Example/script.js
+++ 02_JavaScript_Basics/09_ATM_Example/script.js
@@ -16,19 +16,63 @@ const readlineAsync = ( ) => {
 };
 
 
-console.log("1. Einzahlen");
-console.log("2. Abheben");
-console.log("3. Kontostand");
-console.log("4. Beenden");
+let balance = 0;
 
-console.log("Wählen Sie die gewünschte Aktion aus, indem Sie eine Zahl (1-4) eingeben");
 
-while(true){
-    let inputOfUser = await readlineAsync();
+async function main() {
+while (true) {
+    showMenu();
 
-if (inputOfUser == 1){
-    console.log("Tippen Sie den Betrag ein, welchen Sie einzahlen möchten :")
+let inputOfUser = await readlineAsync();
 
-} 
+
+
+if (inputOfUser == "1"){
+    await einzahlen();
+} else if (inputOfUser == "2"){
+    await abheben();
+} else if (inputOfUser == "3"){
+    kontostand();
+} else if (inputOfUser == "4") {
+    exit();
+}
+}}
+
+function showMenu(){
+    console.log("1. Einzahlen");
+    console.log("2. Abheben");
+    console.log("3. Kontostand");
+    console.log("4. Beenden");
+    
+    console.log("Wählen Sie die gewünschte Aktion aus, indem Sie eine Zahl (1-4) eingeben");
+    }
+
+async function einzahlen(){
+    console.log("Wählen Sie den Betrag, welchen Sie einzahlen wollen: ");
+    let einzuzahlenderBetrag = await readlineAsync();
+    balance += parseFloat(einzuzahlenderBetrag)
+    console.log("\n Sie haben " + einzuzahlenderBetrag + "€ eingezahlt");
+    console.log();
+    
+
 }
 
+async function abheben() {
+    console.log("Wählen Sie den Betrag, welchen Sie abheben wollen: ");
+    let abzuhebenderBetrag = await readlineAsync();
+    balance -= parseFloat(abzuhebenderBetrag);
+    console.log("\n Sie haben " + abzuhebenderBetrag + "€ abgehoben");
+    console.log();
+}
+
+async function kontostand() {
+    console.log("Dies ist Ihr jetziger Konstostand: " + balance + "€ " );
+    console.log();
+}
+
+async function exit() {
+    console.log("Auf Wiedersehen!")
+    readline.close();
+}
+
+main();
