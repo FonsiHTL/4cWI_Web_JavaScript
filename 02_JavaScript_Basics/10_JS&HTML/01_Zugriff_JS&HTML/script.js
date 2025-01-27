c2690276f9ae7546086f42b1d7a9876156efda6f
Leander <leander.wachter@student.htldornbirn.at>
2025-01-27 16:35:12 +0100

    10_HTML_JS started mini calculator

--- 02_JavaScript_Basics/10_JS&HTML/01_Zugriff_JS&HTML/script.js
+++ 02_JavaScript_Basics/10_JS&HTML/01_Zugriff_JS&HTML/script.js
@@ -1,9 +1,15 @@
-document.getElementById("content").innerHTML = "Tolle Sache";
-
-
-
+//document.getElementById("content").innerHTML = "Tolle Sache";
 
 /*let element = document.getElementById("content");
 element.innerHTML = "Tolle Sache";
 */
-console.log(element);
+function buttonClicked() {
+    console.log("clicked");
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+    
+    let result = parseFloat(num1) + parseFloat(num2);
+    console.log(result);
+
+  
+}   
