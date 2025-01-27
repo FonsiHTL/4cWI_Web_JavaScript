bd0a153d3f48ca2a83c146e562263f3c08593288
Leander <leander.wachter@student.htldornbirn.at>
2025-01-27 15:27:33 +0100

    10_HTML_JS started

---
+++ 02_JavaScript_Basics/10_JS&HTML/01_Zugriff_JS&HTML/script.js
@@ -0,0 +1,9 @@
+document.getElementById("content").innerHTML = "Tolle Sache";
+
+
+
+
+/*let element = document.getElementById("content");
+element.innerHTML = "Tolle Sache";
+*/
+console.log(element);
