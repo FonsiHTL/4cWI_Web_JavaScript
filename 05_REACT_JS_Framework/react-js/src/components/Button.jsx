2616af478f61b1db1f64cd99b0f52d7d430050f6
Leander <leander.wachter@student.htldornbirn.at>
2025-03-14 10:39:51 +0100

    02_components worked on button

--- 05_REACT_JS_Framework/react-js/src/components/Button.jsx
+++ 05_REACT_JS_Framework/react-js/src/components/Button.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
-export default function Button() {
-    return <button className="border w-full">
-       sendar
-        
+export default function Button({ title, onClick, enabled }) {
+    const [isGreen, setIsGreen] = useState(enabled);
+  
+    return (
+      <div>
+        <button
+          className={`border w-40 m-7 p-4 text-white font-bold ${
+            isGreen ? "bg-green-400" : "bg-gray-400"
+          }`}
+
+          
+          onClick={() => {
+            setIsGreen(!isGreen); 
+            if (onClick) onClick(); 
+          }}
+        >
+        <div>
+          {title}
+        </div>
+
         </button>
-}
+      </div>
+    );
+  }
--- 05_REACT_JS_Framework/react-js/src/components/Card.jsx
+++ 05_REACT_JS_Framework/react-js/src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 export default function Card({name, jobtitle, description}) {
     return <div className="bg-orange-200 border w-full">
         <p className="font-bold">{name}</p>
