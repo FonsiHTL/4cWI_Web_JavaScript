fbc7c8f357dc6a47c32d3486a8397b0f62985c67
Leander <leander.wachter@student.htldornbirn.at>
2025-04-11 10:12:10 +0200

    06_Football Website, added some styling with trae

--- 05_REACT_JS_Framework/react-js/src/components/FootballTeams.jsx
+++ 05_REACT_JS_Framework/react-js/src/components/FootballTeams.jsx
@@ -2,19 +2,34 @@ import React from 'react'
 
 export default function FootballTeams({ teamData }) {
   return (
-    <div className='bg-blue-200 border-2 border-black m-2 font-mono p-4'>
+    <div className='bg-gradient-to-r from-blue-100 to-blue-200 border-2 
+      border-blue-300 m-2 font-mono p-6 
+      hover:from-blue-200 hover:to-blue-300 hover:shadow-xl 
+      transition-all duration-300 hover:scale-102 
+      cursor-pointer rounded-xl'>
       <div>
         <h2 className='font-bold flex items-center justify-between w-full'>
           <div>
-            <div className='text-xl mb-2'>{teamData.teamName}</div>
-            <div><p>Punkte: {teamData.points}</p></div>
-            <div><p>Tore: {teamData.goals}</p></div>
-            <div><p>Tordifferenz: {teamData.goalDiff}</p></div>
+            <div className='text-xl mb-3 text-blue-900 font-semibold'>{teamData.teamName}</div>
+            <div className='space-y-2'>
+              <p className='flex items-center'>
+                <span className='w-24 text-blue-700'>Punkte:</span>
+                <span className='font-bold'>{teamData.points}</span>
+              </p>
+              <p className='flex items-center'>
+                <span className='w-24 text-blue-700'>Tore:</span>
+                <span className='font-bold'>{teamData.goals}</span>
+              </p>
+              <p className='flex items-center'>
+                <span className='w-24 text-blue-700'>Tordiff.:</span>
+                <span className='font-bold'>{teamData.goalDiff}</span>
+              </p>
+            </div>
           </div>
           <img 
             src={teamData.teamIconUrl} 
             alt="Team Logo" 
-            className='w-16 h-16 object-contain'
+            className='w-20 h-20 object-contain hover:scale-110 transition-transform'
           />
         </h2>
       </div>
